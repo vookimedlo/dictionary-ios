25f74b65a2657953a3b0d318edc640a920ef7fa8
Michal Duda <github@vookimedlo.cz>
2019-06-08 23:16:15 +0200

    Code: About application

---
+++ src/AboutViewController.swift
@@ -0,0 +1,40 @@
+//
+//  AboutViewController.swift
+//  dictionary
+//
+//  Created by Michal Duda on 08/06/2019.
+//  Copyright © 2019 Michal Duda. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func doneButtonAction(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    @IBAction func tapGestureAction(_ sender: UITapGestureRecognizer) {
+        var urlToShow: URL?
+        switch sender.view?.tag {
+        case 1:
+            // TODO: add license url
+            urlToShow = URL(string: "https://github.com/vookimedlo/dictionary-ios")
+        case 2:
+            urlToShow = URL(string: "https://github.com/vookimedlo/dictionary-ios")
+        case 3:
+            urlToShow = URL(string: "https://raw.githubusercontent.com/svobodneslovniky/svobodneslovniky/master/en-cs/LICENCE")
+        case 4:
+            urlToShow = URL(string: "https://github.com/svobodneslovniky/svobodneslovniky")
+        default:
+            debugPrint("Unknown tag", sender.view?.tag)
+        }
+        
+        if let url = urlToShow {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
+    }
+}
