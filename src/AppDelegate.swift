57487e6a1c92bdbbbe41faf4b54bc98978b64bb4
Michal Duda <github@vookimedlo.cz>
2019-10-29 18:20:39 +0100

    🔩Code: Useless menu items removed [catalyst]

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -59,4 +59,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+
+    override func buildMenu(with builder: UIMenuBuilder) {
+        // Mac Catalyst
+        super.buildMenu(with: builder)
+
+        // Menu adjusted to not contain any useless pre-generated items.
+        builder.remove(menu: .services)
+        builder.remove(menu: .format)
+        builder.remove(menu: .toolbar)
+        builder.replaceChildren(ofMenu: .help) { (_) -> [UIMenuElement] in
+            return [UIMenuElement]()
+        }
+    }
 }
